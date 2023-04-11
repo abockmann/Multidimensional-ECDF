2c5b16a43ebddc83df7f2be5106b54e70ccc6133
Arne Bøckmann <abockmann@gmail.com>
2023-04-12 01:44:27 +0200

    multidimensional implementation, not verified, but looks legit

--- dc.py
+++ dc.py
@@ -20,13 +20,35 @@ def rank2(points):
         rank[idxA] = rank_A
         rank[~idxA] = rank_B + numA[~idxA]
         return rank
+    
+def rankn(points):
+    npoints = points.shape[0]
+    if npoints == 1:
+        return 0
+    else:
+        if points.shape[1] == 2:
+            return rank2(points)
+        med = np.median(points[:,0])
+        idxA = points[:,0] <= med
+        A = points[idxA]
+        B = points[~idxA]
+        rank_A = rankn(A)
+        rank_B = rankn(B)
+        rank = np.zeros(npoints, dtype=int)
+        rank[idxA] = rank_A
+        rank[~idxA] = rank_B + rankn(points[:,1:])[~idxA]
+        return rank
 
 if __name__ == "__main__":
+
+    # type "%matplotlib qt" in jupyter input for interactive plots
 
     from matplotlib.pyplot import *
 
     npoints = 1000
-    ndim = 2
+    ndim = 3
     points = (npoints*np.random.random((npoints, ndim)))
-    rank = rank2(points)
-    scatter(points[:,0], points[:,1], c=rank)
+    rank = rankn(points)
+    fig = figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(points[:,0], points[:,1], points[:,2], c=rank)
