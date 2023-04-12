afe75ebcfca90c8e1d9f288b10911568baae82c8
Arne Bøckmann <abockmann@gmail.com>
2023-04-12 13:38:30 +0200

    slight improvement and verification in 2d

--- dc.py
+++ dc.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 
 
 def rank2(points):
@@ -14,7 +14,8 @@ def rank2(points):
         rank_A = rank2(A)
         rank_B = rank2(B)
         idxY = np.argsort(points[:,1])
-        idxYr = np.argsort(idxY)
+        idxYr = np.zeros_like(idxY)
+        idxYr[idxY] = np.arange(idxY.shape[0]) # inverse of idxY
         rank = np.zeros(npoints, dtype=int)
         numA = np.cumsum(idxA[idxY])[idxYr]
         rank[idxA] = rank_A
@@ -28,16 +29,25 @@ def rankn(points):
     else:
         if points.shape[1] == 2:
             return rank2(points)
-        med = np.median(points[:,0])
-        idxA = points[:,0] <= med
-        A = points[idxA]
-        B = points[~idxA]
-        rank_A = rankn(A)
-        rank_B = rankn(B)
-        rank = np.zeros(npoints, dtype=int)
-        rank[idxA] = rank_A
-        rank[~idxA] = rank_B + rankn(points[:,1:])[~idxA]
-        return rank
+    med = np.median(points[:,0])
+    idxA = points[:,0] <= med
+    A = points[idxA]
+    B = points[~idxA]
+    rank_A = rankn(A)
+    rank_B = rankn(B)
+    rank = np.zeros(npoints, dtype=int)
+    rank[idxA] = rank_A
+    rank[~idxA] = rank_B + rankn(points[:,1:])[~idxA]
+    return rank
+    
+def naive(points):
+    # a naive O(n^2) Implementation for validation
+    npoints = points.shape[0]
+    rank = np.zeros(npoints, dtype=int)
+    for i in range(npoints):
+        rank[i] = np.sum(np.all(points[i] > points, axis=1))
+    return rank
+
 
 if __name__ == "__main__":
 
@@ -45,10 +55,19 @@ if __name__ == "__main__":
 
     from matplotlib.pyplot import *
 
-    npoints = 1000
-    ndim = 3
+    npoints = 100000
+    ndim = 2
     points = (npoints*np.random.random((npoints, ndim)))
+    s = time.time()
     rank = rankn(points)
+    print(time.time() - s)
     fig = figure()
     ax = fig.add_subplot(projection='3d')
-    ax.scatter(points[:,0], points[:,1], points[:,2], c=rank)
+    #ax.scatter(points[:,0], points[:,1], points[:,2], c=rank)
+
+    s = time.time()
+    rank_naive = naive(points)
+    print(time.time() - s)
+    #ax.scatter(points[:,0], points[:,1], points[:,2], c=rank_naive)
+
+    # 2d algorithm works perfectly, but not 3d
